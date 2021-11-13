8e758c7e3a39280af2eef6fddff0016bbf42ac35
Lusiker <1044114668@qq.com>
2021-11-13 16:25:20 +0800

    typo found

--- VirtualSlimeSpring/src/main/resources/sql_source/conversation.sql
+++ VirtualSlimeSpring/src/main/resources/sql_source/conversation.sql
@@ -2,7 +2,7 @@ create table conversation(
     cid int auto_increment primary key,
     uid1 int,
     uid2 int,
-    createAt timestamp,
+    createdAt timestamp,
     lastUpdated timestamp null,
 
     foreign key(uid1) references user(uid),
