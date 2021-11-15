49f3c54bd6980e86c4e613443b95809b25451443
Lusiker <1044114668@qq.com>
2021-11-15 15:42:45 +0800

    Annotation @Mapper added to mappers

--- VirtualSlimeSpring/src/main/java/com/virtualSlime/Mapper/AdminMapper.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Mapper/AdminMapper.java
@@ -2,6 +2,8 @@ package com.virtualSlime.Mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.virtualSlime.Entity.Admin;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface AdminMapper extends BaseMapper<Admin> {
 }
--- VirtualSlimeSpring/src/main/java/com/virtualSlime/Mapper/UserMapper.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Mapper/UserMapper.java
@@ -2,6 +2,8 @@ package com.virtualSlime.Mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
 import com.virtualSlime.Entity.User;
+import org.apache.ibatis.annotations.Mapper;
 
+@Mapper
 public interface UserMapper extends BaseMapper<User> {
 }
