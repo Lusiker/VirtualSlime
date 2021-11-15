7153908bf34e4f12985db7edfe215032531398f3
Lusiker <1044114668@qq.com>
2021-11-15 14:38:54 +0800

    Comment structure optimized

--- VirtualSlimeSpring/src/main/resources/sql_source/user_comment.sql
+++ VirtualSlimeSpring/src/main/resources/sql_source/user_comment.sql
@@ -3,10 +3,10 @@ create table user_comment(
     cid int unsigned auto_increment primary key,
     uid int unsigned,
     iid int unsigned,
-    created_at timestamp,
+    created_at timestamp ,
     reply_uid int unsigned null,
-    type tinyint unsigned,
-    content varchar(200),
-    rate tinyint unsigned,
-    state tinyint unsigned default 0
+    comment_type tinyint unsigned,
+    comment_content varchar(200),
+    comment_rate tinyint unsigned,
+    comment_state tinyint unsigned default 0
 );
