be9ce16b9265f7c8d104eb236a0c8c7084217151
Yuuna233 <tuzijiamian@gmail.com>
2021-11-16 20:53:14 +0800

    category

--- VirtualSlimeSpring/src/main/java/com/virtualSlime/Entity/Category.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Entity/Category.java
@@ -1,37 +1,40 @@
 package com.virtualSlime.Entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
+import com.baomidou.mybatisplus.annotation.TableName;
+
+@TableName(schema = "virtual_slime", value = "admin")
 public class Category {
-    private Byte cid;
-    private Byte cidUpper;
+    @TableId(type = IdType.AUTO)
+    private final Short cid;
+    private Short cidUpper;
     private String catName;//<= 20
     private String catBrief;//<= 50
 
-    public Category(Byte cidUpper, String catName, String catBrief) {
+    public Category(Short cidUpper, String catName, String catBrief) {
+        this.cid = null;
         this.cidUpper = cidUpper;
         this.catName = catName;
         this.catBrief = catBrief;
     }
 
-    public Category(Byte cid, Byte cidUpper, String catName, String catBrief) {
+    public Category(Short cid, Short cidUpper, String catName, String catBrief) {
         this.cid = cid;
         this.cidUpper = cidUpper;
         this.catName = catName;
         this.catBrief = catBrief;
     }
 
-    public Byte getCid() {
+    public Short getCid() {
         return cid;
     }
 
-    public void setCid(Byte cid) {
-        this.cid = cid;
-    }
-
-    public Byte getCidUpper() {
+    public Short getCidUpper() {
         return cidUpper;
     }
 
-    public void setCidUpper(Byte cidUpper) {
+    public void setCidUpper(Short cidUpper) {
         this.cidUpper = cidUpper;
     }
 
--- VirtualSlimeSpring/src/main/resources/sql_source/item_category.sql
+++ VirtualSlimeSpring/src/main/resources/sql_source/item_category.sql
@@ -1,7 +1,7 @@
 drop table if exists item_category;
 create table item_category(
-    cid smallint unsigned auto_increment primary key,
-    cid_upper smallint unsigned default null,
+    cid smallint auto_increment primary key,
+    cid_upper smallint default null,
     cat_name varchar(20),
     cat_brief varchar(50)
 );
