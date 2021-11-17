8fe34d0df1506109c6c6d9c11c384f713843afeb
Lusiker <1044114668@qq.com>
2021-11-17 17:16:27 +0800

    Create GlobalConfig.java
    this global configuration is for serializing all controller enums

---
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Config/GlobalConfig.java
@@ -0,0 +1,28 @@
+package com.virtualSlime.Config;
+
+import com.fasterxml.jackson.core.JsonGenerator;
+import com.fasterxml.jackson.databind.JsonSerializer;
+import com.fasterxml.jackson.databind.SerializerProvider;
+import com.virtualSlime.Enum.ControllerStateEnum;
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.io.IOException;
+
+@Configuration
+public class GlobalConfig {
+
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer enumSerializer(){
+        return jacksonObjectMapperBuilder -> jacksonObjectMapperBuilder.serializerByType(ControllerStateEnum.class, new JsonSerializer<ControllerStateEnum>() {
+            @Override
+            public void serialize(ControllerStateEnum stateEnum, JsonGenerator generator, SerializerProvider provider) throws IOException {
+                generator.writeStartObject();
+                generator.writeNumberField("state",stateEnum.getStateCode());
+                generator.writeStringField("value",stateEnum.getValue());
+                generator.writeEndObject();
+            }
+        });
+    }
+}
