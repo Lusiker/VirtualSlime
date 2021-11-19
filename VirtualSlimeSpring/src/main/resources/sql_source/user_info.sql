59a610530bb66d9c32fdf165b939503ccbda1937
Yuuna233 <tuzijiamian@gmail.com>
2021-11-19 20:34:41 +0800

    Update user_info.sql

--- VirtualSlimeSpring/src/main/resources/sql_source/user_info.sql
+++ VirtualSlimeSpring/src/main/resources/sql_source/user_info.sql
@@ -1,13 +1,13 @@
 drop table if exists user_info;
 create table user_info(
     uid int auto_increment primary key,
-    user_name varchar(20),
-    user_email varchar(50),
-    user_password tinytext,
+    user_name varchar(20) not null,
+    user_email varchar(50) not null,
+    user_password tinytext not null,
     user_introduction varchar(50) default '编辑个人简介',
-    created_at timestamp null default current_timestamp,
+    created_at timestamp default current_timestamp,
     last_login timestamp default null,
-    total_login int default 0,
+    total_login int default 0 not null,
     user_birthday date default null,
     user_sex tinyint default 0,
     user_show_birthday boolean default true,
