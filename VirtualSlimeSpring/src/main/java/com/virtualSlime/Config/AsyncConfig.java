8ee3d118ba88d77f27821b446dd92aaa14bcfd8a
Lusiker <1044114668@qq.com>
2021-11-19 14:06:14 +0800

    Enabled async

---
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Config/AsyncConfig.java
@@ -0,0 +1,23 @@
+package com.virtualSlime.Config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.task.AsyncTaskExecutor;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+
+@Configuration
+public class AsyncConfig {
+    private static final int MAX_POOL_SIZE = 50;
+    private static final int CORE_POOL_SIZE = 20;
+
+    @Bean("asyncTaskExecutor")
+    public AsyncTaskExecutor asyncTaskExecutor(){
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setMaxPoolSize(MAX_POOL_SIZE);
+        executor.setCorePoolSize(CORE_POOL_SIZE);
+        executor.setThreadNamePrefix("vs-thread-");
+        executor.initialize();
+
+        return executor;
+    }
+}
--- VirtualSlimeSpring/src/main/java/com/virtualSlime/VirtualSlimeApplication.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/VirtualSlimeApplication.java
@@ -3,8 +3,10 @@ package com.virtualSlime;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 @MapperScan("com.virtualSlime.Mapper")
 public class VirtualSlimeApplication {
 
