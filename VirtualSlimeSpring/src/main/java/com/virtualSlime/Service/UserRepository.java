2c0314fe1327b3f36ef00d8a6086b99765c3a4c7
Lusiker <1044114668@qq.com>
2021-11-19 14:06:49 +0800

    Update UserRepository.java

--- VirtualSlimeSpring/src/main/java/com/virtualSlime/Service/UserRepository.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Service/UserRepository.java
@@ -3,6 +3,7 @@ package com.virtualSlime.Service;
 import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.baomidou.mybatisplus.core.conditions.update.UpdateWrapper;
 import com.virtualSlime.Entity.User;
+import com.virtualSlime.Enum.UserState;
 import com.virtualSlime.Mapper.UserMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -34,6 +35,29 @@ public class UserRepository {
         }else{
             user.setTotalLogin(userTotalLogin + 1);
         }
+
+        return userMapper.update(user, wrapper) == 1;
+    }
+
+    public boolean updateUserHasActivatedTrue(User user){
+        //set user's hasActivated to true and state to NORMAL
+        UpdateWrapper<User> wrapper = new UpdateWrapper<User>();
+        wrapper.eq("uid",user.getUid());
+
+        user.setUserHasActivated(true);
+        user.setUserState(UserState.NORMAL);
+
+        return userMapper.update(user, wrapper) == 1;
+    }
+
+    public boolean updateUserResetEmail(User user, String newEmail){
+        //set user's email to new email, state to restricted and hasActivated to false
+        UpdateWrapper<User> wrapper = new UpdateWrapper<User>();
+        wrapper.eq("uid",user.getUid());
+
+        user.setUserEmail(newEmail);
+        user.setUserHasActivated(false);
+        user.setUserState(UserState.RESTRICTED);
 
         return userMapper.update(user, wrapper) == 1;
     }
