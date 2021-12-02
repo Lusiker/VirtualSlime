3a6f86932ab656de153bcbcb8500930f8bd10055
Lusiker <1044114668@qq.com>
2021-12-02 22:59:59 +0800

    Create ProfileState.java
    enum for profile controller

---
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Enum/ProfileState.java
@@ -0,0 +1,28 @@
+package com.virtualSlime.Enum;
+
+public enum ProfileState implements ControllerStateEnum{
+    FAILED(-1,"failed"),
+    ACCESS_DENIED(-1,"access_denied"),
+    AS_MASTER(0,"as_master"),
+    AS_GUEST(1,"as_guest"),
+    UPDATE_SUCCESSFUL(2,"update_successful"),
+    UPDATE_FAILED(3,"update_failed");
+
+    private final int stateCode;
+    private final String value;
+
+    ProfileState(int stateCode, String value){
+        this.stateCode = stateCode;
+        this.value = value;
+    }
+
+    @Override
+    public int getStateCode() {
+        return this.stateCode;
+    }
+
+    @Override
+    public String getValue() {
+        return this.value;
+    }
+}
