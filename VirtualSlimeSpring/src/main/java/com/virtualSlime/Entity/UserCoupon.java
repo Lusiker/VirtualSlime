76f42c47b0006d1586ec5e0afa833266c3a6001c
Lusiker <1044114668@qq.com>
2021-12-14 20:39:41 +0800

    Update UserCoupon.java

--- VirtualSlimeSpring/src/main/java/com/virtualSlime/Entity/UserCoupon.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Entity/UserCoupon.java
@@ -1,5 +1,7 @@
 package com.virtualSlime.Entity;
 
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
 import lombok.Getter;
 import lombok.Setter;
@@ -10,6 +12,7 @@ import lombok.ToString;
 @ToString
 @TableName(schema = "virtual_slime", value = "r_user_have_coupon")
 public class UserCoupon {
+    @TableId(type = IdType.AUTO)
     private Integer uid;
     private Integer cid;
 }
