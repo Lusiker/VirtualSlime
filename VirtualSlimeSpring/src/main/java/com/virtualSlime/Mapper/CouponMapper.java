c629b6edee9f242c4540a2fab4c4d68a4d401625
Lusiker <1044114668@qq.com>
2021-12-14 20:40:05 +0800

    Create CouponMapper.java

---
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Mapper/CouponMapper.java
@@ -0,0 +1,9 @@
+package com.virtualSlime.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.virtualSlime.Entity.Coupon;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface CouponMapper extends BaseMapper<Coupon>{
+}
