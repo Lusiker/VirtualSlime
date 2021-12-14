93949af9b49ef9b2b45cc3b2ee2c6ab1317f117b
Lusiker <1044114668@qq.com>
2021-12-14 20:39:54 +0800

    Create GlobalCategoryCache.java

---
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Utils/GlobalCategoryCache.java
@@ -0,0 +1,31 @@
+package com.virtualSlime.Utils;
+
+import com.virtualSlime.Entity.Category;
+import com.virtualSlime.Mapper.CategoryMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.List;
+
+@Component
+public class GlobalCategoryCache {
+    private final HashMap<Short, Category> map = new HashMap<Short,Category>();
+
+    public GlobalCategoryCache(@Autowired CategoryMapper categoryMapper){
+        List<Category> list = categoryMapper.selectList(null);
+        for(Category cat : list){
+            map.put(cat.getCid(),cat);
+        }
+    }
+
+    public String getCategoryNameFromCid(short cid){
+        Category cat = map.get(cid);
+        if(cat == null){
+            return "undefined";
+        }
+
+        return cat.getName();
+    }
+}
