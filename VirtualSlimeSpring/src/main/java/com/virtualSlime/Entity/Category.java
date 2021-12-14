03c9ce761c863ec032fbf3017921a757e237f6be
Lusiker <1044114668@qq.com>
2021-12-14 20:39:34 +0800

    Update Category.java

--- VirtualSlimeSpring/src/main/java/com/virtualSlime/Entity/Category.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Entity/Category.java
@@ -3,64 +3,18 @@ package com.virtualSlime.Entity;
 import com.baomidou.mybatisplus.annotation.IdType;
 import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import lombok.Getter;
+import lombok.Setter;
+import lombok.ToString;
 
-@TableName(schema = "virtual_slime", value = "admin")
+@Getter
+@Setter
+@ToString
+@TableName(schema = "virtual_slime", value = "item_category")
 public class Category {
     @TableId(type = IdType.AUTO)
-    private final Short cid;
-    private Short cidUpper;
-    private String catName;//<= 20
-    private String catBrief;//<= 50
-
-    public Category(Short cidUpper, String catName, String catBrief) {
-        this.cid = null;
-        this.cidUpper = cidUpper;
-        this.catName = catName;
-        this.catBrief = catBrief;
-    }
-
-    public Category(Short cid, Short cidUpper, String catName, String catBrief) {
-        this.cid = cid;
-        this.cidUpper = cidUpper;
-        this.catName = catName;
-        this.catBrief = catBrief;
-    }
-
-    public Short getCid() {
-        return cid;
-    }
-
-    public Short getCidUpper() {
-        return cidUpper;
-    }
-
-    public void setCidUpper(Short cidUpper) {
-        this.cidUpper = cidUpper;
-    }
-
-    public String getCatName() {
-        return catName;
-    }
-
-    public void setCatName(String catName) {
-        this.catName = catName;
-    }
-
-    public String getCatBrief() {
-        return catBrief;
-    }
-
-    public void setCatBrief(String catBrief) {
-        this.catBrief = catBrief;
-    }
-
-    @Override
-    public String toString() {
-        return "Category{" +
-                "cid=" + cid +
-                ", cidUpper=" + cidUpper +
-                ", catName='" + catName + '\'' +
-                ", catBrief='" + catBrief + '\'' +
-                '}';
-    }
+    private Short cid;
+    private Short belongsTo;
+    private String name;//<= 20
+    private String brief;//<= 50
 }
