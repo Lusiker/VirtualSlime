6fc64a235003c4a256578d3a208dfdd9d15ea20c
Lusiker <1044114668@qq.com>
2021-12-15 18:34:13 +0800

    Update UserCouponMapper.java

--- VirtualSlimeSpring/src/main/java/com/virtualSlime/Mapper/UserCouponMapper.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Mapper/UserCouponMapper.java
@@ -1,7 +1,7 @@
 package com.virtualSlime.Mapper;
 
 import com.baomidou.mybatisplus.core.mapper.BaseMapper;
-import com.virtualSlime.Entity.UserCoupon;
+import com.virtualSlime.Entity.Relation.UserCoupon;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
