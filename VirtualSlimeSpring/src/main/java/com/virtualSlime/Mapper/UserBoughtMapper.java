9df235ae7ab255a344503fc26476749c90ffee03
Lusiker <1044114668@qq.com>
2021-12-15 18:34:21 +0800

    Create UserBoughtMapper.java

---
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Mapper/UserBoughtMapper.java
@@ -0,0 +1,9 @@
+package com.virtualSlime.Mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.virtualSlime.Entity.Relation.UserBought;
+import org.apache.ibatis.annotations.Mapper;
+
+@Mapper
+public interface UserBoughtMapper extends BaseMapper<UserBought> {
+}
