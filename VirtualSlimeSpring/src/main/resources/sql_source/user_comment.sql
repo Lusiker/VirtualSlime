9483007bdf2f23eb6b68f57d4f74aeb2eab2c081
Lusiker <1044114668@qq.com>
2021-12-16 21:31:06 +0800

    Update user_comment.sql
    deleted reply part

--- VirtualSlimeSpring/src/main/resources/sql_source/user_comment.sql
+++ VirtualSlimeSpring/src/main/resources/sql_source/user_comment.sql
@@ -3,9 +3,7 @@ create table user_comment(
     cid int unsigned auto_increment primary key,
     uid int unsigned,
     iid int unsigned,
-    created_at timestamp ,
-    reply_uid int unsigned null,
-    comment_type tinyint unsigned,
+    created_at timestamp,
     comment_content varchar(200),
     comment_rate tinyint unsigned,
     comment_state tinyint unsigned default 0
