e6cd72b71d95a369c49618b010f1f0cd63dc92ac
Lusiker <1044114668@qq.com>
2021-12-17 20:48:57 +0800

    ？？？？

---
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Enum/EntityType/CommentState.java
@@ -0,0 +1,40 @@
+package com.virtualSlime.Enum.EntityType;
+
+import com.baomidou.mybatisplus.annotation.EnumValue;
+import com.baomidou.mybatisplus.annotation.IEnum;
+
+public enum CommentState implements IEnum<Byte> {
+    NORMAL(0),
+    RESTRICTED(1),
+    HIDDEN(2);
+
+    @EnumValue
+    private final Byte value;
+
+    CommentState(int value){
+        this.value = (byte)value;
+    }
+
+    @Override
+    public Byte getValue(){
+        return this.value;
+    }
+
+    public enum CommentType implements IEnum<Byte> {
+        UNDEFINED(0),
+        RATING(1),
+        REPLY(2);
+    
+        @EnumValue
+        private final Byte value;
+    
+        CommentType(int value) {
+            this.value = (byte)value;
+        }
+    
+        @Override
+        public Byte getValue() {
+            return this.value;
+        }
+    }
+}
