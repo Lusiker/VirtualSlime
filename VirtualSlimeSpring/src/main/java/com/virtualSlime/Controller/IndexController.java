492f85b700bd585d97f1388c4ffcee35e2072497
Lusiker <1044114668@qq.com>
2021-12-18 17:09:12 +0800

    Update IndexController.java
    -you can now search items or users by name

--- VirtualSlimeSpring/src/main/java/com/virtualSlime/Controller/IndexController.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Controller/IndexController.java
@@ -12,6 +12,7 @@ import com.virtualSlime.Service.UserRepository;
 import com.virtualSlime.Utils.GlobalCategoryCache;
 import com.virtualSlime.Utils.InfoWrapper.ItemInfoWrapper;
 import com.virtualSlime.Utils.InfoWrapper.Result;
+import com.virtualSlime.Utils.InfoWrapper.UserInfoWrapper;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -48,21 +49,41 @@ public class IndexController {
         return processedItems;
     }
 
-    @RequestMapping("/home")
-    public String index(@RequestParam(value = "page",defaultValue = "")String newPage) throws JsonProcessingException {
-        int page;
-        if(newPage.equals("")){
-            page = 1;
+    private List<UserInfoWrapper> getUserInfoWrapperList(List<User> list){
+        List<UserInfoWrapper> processedItems = new ArrayList<>();
+
+        for(User u : list){
+            UserInfoWrapper wrapper = new UserInfoWrapper.UserInfoWrapperBuilder()
+                    .setLesserInfo(u)
+                    .build();
+
+            processedItems.add(wrapper);
+        }
+
+        return processedItems;
+    }
+
+    private int parsePageString(String page){
+        if(page.equals("")){
+            return 1;
         }else{
+            int result;
             try{
-                page = Integer.parseInt(newPage);
-                if(page <= 0){
-                    page = 1;
+                result = Integer.parseInt(page);
+                if(result <= 0){
+                    return 1;
                 }
+
+                return result;
             }catch (Exception e){
-                return objectMapper.writeValueAsString(new Result(ProfilePageState.FAILED, newPage));
+                return 1;
             }
         }
+    }
+
+    @RequestMapping("/home")
+    public String index(@RequestParam(value = "page",defaultValue = "")String newPage) throws JsonProcessingException {
+        int page = parsePageString(newPage);
 
         List<Item> items = itemRepository.selectAvailableItemsByPage(page,10);
         List<ItemInfoWrapper> processedItems = getItemInfoWrapperList(items);
@@ -71,25 +92,13 @@ public class IndexController {
             return objectMapper.writeValueAsString(new Result(IndexPageState.SUCCESSFUL,processedItems));
         }
 
-        return objectMapper.writeValueAsString(new Result(IndexPageState.FAILED,null));
+        return objectMapper.writeValueAsString(new Result(IndexPageState.NO_MORE,null));
     }
 
     @RequestMapping("/home/cid={cid}")
     public String indexWithCategory(@PathVariable(value = "cid")String newCid,
                                     @RequestParam(value = "page")String newPage) throws JsonProcessingException{
-        int page;
-        if(newPage.equals("")){
-            page = 1;
-        }else{
-            try{
-                page = Integer.parseInt(newPage);
-                if(page <= 0){
-                    page = 1;
-                }
-            }catch (Exception e){
-                return objectMapper.writeValueAsString(new Result(ProfilePageState.FAILED, newPage));
-            }
-        }
+        int page = parsePageString(newPage);
 
         int cid;
         try{
@@ -105,7 +114,35 @@ public class IndexController {
             return objectMapper.writeValueAsString(new Result(IndexPageState.SUCCESSFUL,processedItems));
         }
 
-        return objectMapper.writeValueAsString(new Result(IndexPageState.FAILED,null));
+        return objectMapper.writeValueAsString(new Result(IndexPageState.NO_MORE,null));
+    }
+
+    @RequestMapping("/search/item")
+    public String searchUserSimple(@RequestParam(value = "keyword",defaultValue = "")String newKeyword,
+                                   @RequestParam(value = "page",defaultValue = "")String newPage) throws JsonProcessingException{
+        int page = parsePageString(newPage);
+        List<Item> items = itemRepository.selectAvailableItemsLikeKeywordByPage(newKeyword,page,10);
+        List<ItemInfoWrapper> processedItems = getItemInfoWrapperList(items);
+
+        if(processedItems.size() != 0) {
+            return objectMapper.writeValueAsString(new Result(IndexPageState.SUCCESSFUL,processedItems));
+        }
+
+        return objectMapper.writeValueAsString(new Result(IndexPageState.NO_MORE,null));
+    }
+
+    @RequestMapping("/search/user")
+    public String searchItemSimple(@RequestParam(value = "keyword",defaultValue = "")String newKeyword,
+                                   @RequestParam(value = "page",defaultValue = "")String newPage) throws JsonProcessingException{
+        int page = parsePageString(newPage);
+        List<User> users = userRepository.selectAvailableUsersLikeKeywordByPage(newKeyword,page,10);
+        List<UserInfoWrapper> processedUsers = getUserInfoWrapperList(users);
+
+        if(processedUsers.size() != 0) {
+            return objectMapper.writeValueAsString(new Result(IndexPageState.SUCCESSFUL,processedUsers));
+        }
+
+        return objectMapper.writeValueAsString(new Result(IndexPageState.NO_MORE,null));
     }
 
     /**
