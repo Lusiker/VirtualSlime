d991089ac08d1475ab32a18b563d0279e50f8ad2
Lusiker <1044114668@qq.com>
2021-12-19 18:23:33 +0800

    Update GlobalDefaultAvatarCache.java

--- VirtualSlimeSpring/src/main/java/com/virtualSlime/Utils/InfoWrapper/GlobalDefaultAvatarCache.java
+++ VirtualSlimeSpring/src/main/java/com/virtualSlime/Utils/InfoWrapper/GlobalDefaultAvatarCache.java
@@ -9,6 +9,15 @@ import java.io.*;
 public class GlobalDefaultAvatarCache {
     private static final String targetPath = "../VirtualSlimeVue/src/assets/user/";
     private static final String resPath = "./src/main/resources/static/default_avatar.jpg";
+    private static FileInputStream defaultAvatar = null;
+
+    static {
+        try {
+            defaultAvatar = new FileInputStream(resPath);
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
 
     public static void createAvatar(User user) throws IOException {
         String finalPath = targetPath + user.getUid();
@@ -16,11 +25,10 @@ public class GlobalDefaultAvatarCache {
         File dir = new File(finalPath);
         dir.mkdirs();
 
-        FileInputStream in = new FileInputStream(resPath);
         FileOutputStream out = new FileOutputStream(finalPath + "/avatar.jpg");
         byte[] buf = new byte[1024];
         int bytesRead;
-        while ((bytesRead = in.read(buf)) != -1) {
+        while ((bytesRead = defaultAvatar.read(buf)) != -1) {
             out.write(buf, 0, bytesRead);
         }
     }
